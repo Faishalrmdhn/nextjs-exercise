408a3f6226282c8d0a9215bf9ceaef2d14c70b86
Faishalrmdhn <faishalramadhan25@gmail.com>
2023-08-03 17:40:33 +0700

    pre-rendering and data fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,23 @@ import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home () {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+// SSG and SSR cannot be use at the same time
+// export async function getServerSideProps(){
+//   return 'something'
+// }
+
+export default function Home ({allPostsData}:any) {
   return (  
     <Layout home>
       <Head>
@@ -17,6 +32,21 @@ export default function Home () {
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, dignissimos.
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }:any) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
